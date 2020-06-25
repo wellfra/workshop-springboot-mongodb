9444818ddd356bc819f16e3d3eb8a7a576e45de6
wellfra <well_souza_santos@yahoo.com.br>
2020-06-25 16:32:51 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/wellfra/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellfra.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
